cf72074fda88c3452964cdc9bc0c16dfba4c54bd
Dzmitry Miatelitsa <Dzmitry>
2022-02-23 11:18:33 +0300

    task finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let result = str;
+  const brackets = bracketsConfig.map((b) => b.join(""));
+  let repeat = true;
+  while (repeat) {
+    let tmp = result;
+    brackets.forEach((b) => {
+      result = result.replace(b, "");
+    });
+    if (result === tmp) {
+      repeat = false;
+    } else {
+      repeat = true;
+    }
+  }
+  return !!!result;
+};
